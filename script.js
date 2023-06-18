6881a00b88679a9452bb039d6f55b83c1fb4450b
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-18 19:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function volume_sphere() {
 	let r = document.getElementById('radius').value;
 	const pi = Math.PI;
-	let val = ((4/3) * pi) * (r * r * r);
-	document.getElementById('volume').value = val;
+	let val = (4/3) * pi * (r * r * r);
+	let ans = val.toFixed(4);
+	document.getElementById('volume').value = ans;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
